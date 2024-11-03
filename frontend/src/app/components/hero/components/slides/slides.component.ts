c991c245cc3804e4156c37ed9ada4856bcf29a0f
whereishikaru <h.suzuki.028@gmail.com>
2024-11-04 09:27:12 +1300

    feat: implement slides

--- frontend/src/app/components/hero/components/slides/slides.component.ts
+++ frontend/src/app/components/hero/components/slides/slides.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-slides',
   standalone: true,
   imports: [],
   templateUrl: './slides.component.html',
-  styleUrl: './slides.component.scss'
+  styleUrl: './slides.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SlidesComponent {
 
--- frontend/src/app/components/hero/hero.component.ts
+++ frontend/src/app/components/hero/hero.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { SlidesComponent } from "./components/slides/slides.component";
-import { SlideTogglerComponent } from "./components/slide-toggler/slide-toggler.component";
 import { LanguagesComponent } from "./components/languages/languages.component";
 import { LogoComponent } from "./components/logo/logo.component";
 import { MenuComponent } from "./components/menu/menu.component";
@@ -8,7 +7,7 @@ import { MenuComponent } from "./components/menu/menu.component";
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [SlidesComponent, SlideTogglerComponent, LanguagesComponent, LogoComponent, MenuComponent],
+  imports: [SlidesComponent, LanguagesComponent, LogoComponent, MenuComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
