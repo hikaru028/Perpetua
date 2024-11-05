6652ef876629eacbb5b4080ba45745f8ff014b5d
whereishikaru <h.suzuki.028@gmail.com>
2024-11-05 15:12:10 +1300

    refactor: sort button effect

--- frontend/src/app/components/projects/components/project-card/project-card.component.ts
+++ frontend/src/app/components/projects/components/project-card/project-card.component.ts
@@ -1,7 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StrapiService } from '../../../../services/strapi.service';
-import { IProject, APIResponseModel } from '../../../../../util/interfaces';
+import { IProject } from '../../../../../util/interfaces';
 
 @Component({
   selector: 'app-project-card',
@@ -10,25 +9,6 @@ import { IProject, APIResponseModel } from '../../../../../util/interfaces';
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss']
 })
-export class ProjectCardComponent implements OnInit {
-  projects: IProject[] = [];
-  strapiUrl = 'http://localhost:1337';
-  strapiService = inject(StrapiService);
-
-  ngOnInit(): void {
-    this.strapiService.getAllProjects().subscribe((result: APIResponseModel) => {
-      this.projects = result.data;
-
-      this.projects = this.projects.map(project => ({
-        ...project,
-        project_image: {
-          ...project.project_image,
-          url: this.strapiUrl + project.project_image.url || "../../../../../assets/images/img_n.a.png"
-        }
-      }));
-
-    }, error => {
-      console.error('Error fetching projects:', error);
-    });
-  }
+export class ProjectCardComponent {
+  @Input() projects: IProject[] = [];
 }
--- frontend/src/app/components/projects/projects.component.ts
+++ frontend/src/app/components/projects/projects.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProjectCardComponent } from "./components/project-card/project-card.component";
+import { IProject, APIResponseModel } from '../../../util/interfaces';
+import { StrapiService } from '../../services/strapi.service';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [ProjectCardComponent],
+  imports: [CommonModule, ProjectCardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
 
+export class ProjectsComponent implements OnInit {
+  projects: IProject[] = [];
+  filteredProjects: IProject[] = [];
+  selectedFilter: string | null = null;
+  strapiService = inject(StrapiService);
+  strapiUrl = 'http://localhost:1337';
+
+  ngOnInit(): void {
+    this.strapiService.getAllProjects().subscribe((result: APIResponseModel) => {
+      this.projects = result.data;
+      this.projects = this.projects.map(project => ({
+        ...project,
+        project_image: {
+          ...project.project_image,
+          url: this.strapiUrl + project.project_image.url || "../../../../../assets/images/img_n.a.png"
+        }
+      }));
+      this.filteredProjects = [...this.projects];
+    }, error => {
+      console.error('Error fetching projects:', error);
+    });
+  }
+
+  sortProjects(type: string): void {
+    if (this.selectedFilter === type) {
+      this.selectedFilter = null;
+      this.filteredProjects = this.projects;
+    } else {
+      this.selectedFilter = type;
+      this.filteredProjects = type === '' ? this.projects : this.projects.filter(project => project.project_type === type);
+    }
+  }
 }
