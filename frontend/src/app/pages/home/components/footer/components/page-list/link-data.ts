eb33e11a364c46e5290674c6096e92a1aa923ceb
whereishikaru <h.suzuki.028@gmail.com>
2024-11-06 22:37:29 +1300

    feat: add link data file

---
+++ frontend/src/app/pages/home/components/footer/components/page-list/link-data.ts
@@ -0,0 +1,125 @@
+export const ServiceData: any[] = [
+    {
+        section: 'Projects',
+        items: [
+            {
+                top: [
+                    {
+                        title: 'All projects',
+                        path: '/projects'
+                    },
+                    {
+                        title: 'Browse by type',
+                        path: '/projects'
+                    },
+                    {
+                        title: 'Browse by industry',
+                        path: '/projects'
+                    }
+                ],
+                bottom: [
+                    {
+                        title: 'MyOCP App',
+                        path: '/projects'
+                    },
+                    {
+                        title: 'Duncan Taylor Builders Website',
+                        path: '/projects'
+                    },
+                    {
+                        title: 'Perpetua Website',
+                        path: '/projects'
+                    },
+                    {
+                        title: 'OCP Group Website',
+                        path: '/projects'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        section: 'What we do',
+        items: [
+            {
+                title: 'Custom software',
+                path: '/services',
+            },
+            {
+                title: 'Websites & CMS',
+                path: '/services',
+            },
+            {
+                title: 'Mobile & native apps',
+                path: '/services',
+            },
+            {
+                title: 'Artificial intelligence',
+                path: '/services',
+            },
+            {
+                title: 'Hosting & cloud services',
+                path: '/services',
+            },
+            {
+                title: 'Data & analytics',
+                path: '/services',
+            }
+        ]
+    },
+    {
+        section: 'About us',
+        items: [
+            {
+                title: 'Our team',
+                path: '/about',
+            },
+            {
+                title: 'Our history',
+                path: '/about',
+            },
+            {
+                title: 'Our clients',
+                path: '/about',
+            },
+            {
+                title: 'Testimonials',
+                path: '/about',
+            }
+        ]
+    },
+    {
+        section: 'Blog',
+        items: [
+            {
+                title: 'All content',
+                path: '/blog',
+            },
+            {
+                title: 'News posts',
+                path: '/blog',
+            },
+            {
+                title: 'Articles',
+                path: '/blog',
+            }
+        ]
+    },
+    {
+        section: 'Contact',
+        items: [
+            {
+                title: 'Send us a message',
+                path: '/contact',
+            },
+            {
+                title: 'Our locations',
+                path: '/contact',
+            },
+            {
+                title: 'Careers',
+                path: '/contact',
+            }
+        ]
+    }
+];
--- frontend/src/app/pages/home/components/footer/footer.component.ts
+++ frontend/src/app/pages/home/components/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ContactInfoComponent } from './components/contact-info/contact-info.component';
+import { PageListComponent } from './components/page-list/page-list.component';
+import { LanguagesComponent } from './components/languages/languages.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, ContactInfoComponent, PageListComponent, LanguagesComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
