d83da6cb88ab1b2dca7326033ad73c84903597d1
whereishikaru <h.suzuki.028@gmail.com>
2024-11-11 17:22:54 +1300

    refactor: add projecct content section

--- frontend/src/app/pages/projects/project-detail/components/project-content/project-content.component.ts
+++ frontend/src/app/pages/projects/project-detail/components/project-content/project-content.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+// Libraries
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// Components
+import { IProject } from '../../../../../../util/interfaces'
 
 @Component({
   selector: 'app-project-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-content.component.html',
   styleUrl: './project-content.component.scss'
 })
 export class ProjectContentComponent {
-
+  @Input() project: IProject | undefined;
 }
