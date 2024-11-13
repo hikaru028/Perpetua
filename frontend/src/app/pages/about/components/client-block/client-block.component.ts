18077a573ce6768fbb7b7d9f61e3f01ddd076b84
whereishikaru <h.suzuki.028@gmail.com>
2024-11-14 08:47:48 +1300

    fix: page rendering button

--- frontend/src/app/pages/about/components/client-block/client-block.component.ts
+++ frontend/src/app/pages/about/components/client-block/client-block.component.ts
@@ -1,13 +1,14 @@
 // Libraries
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 // Services
 import { IClient } from '../../../../../util/interfaces';
 
 @Component({
   selector: 'app-client-block',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './client-block.component.html',
   styleUrl: './client-block.component.scss'
 })
