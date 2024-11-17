3eb52911cb9ad4b34456f98f0991544637705ca7
whereishikaru <h.suzuki.028@gmail.com>
2024-11-17 16:01:02 +1300

    translate footer section

--- frontend/src/app/components/buttons/back-to-top-button/back-to-top-button.component.ts
+++ frontend/src/app/components/buttons/back-to-top-button/back-to-top-button.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+// Services
+import { TranslationHelper } from '../../../shared/translation-helper';
 
 @Component({
   selector: 'app-back-to-top-button',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './back-to-top-button.component.html',
   styleUrl: './back-to-top-button.component.scss'
 })
-export class BackToTopButtonComponent {
+export class BackToTopButtonComponent implements OnDestroy {
+  currentLanguage: string = 'en';
+
+  constructor(private translationHelper: TranslationHelper) {
+    this.currentLanguage = this.translationHelper.getCurrentLanguage();
+  }
+
+  ngOnDestroy(): void {
+    this.translationHelper.unsubscribe();
+  }
+
   scrollToTop(): void {
     window.scrollTo({
       top: 0,
--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+// Libraries
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+// Components
 import { ContactInfoComponent } from './components/contact-info/contact-info.component';
 import { PageListComponent } from './components/page-list/page-list.component';
 import { LanguagesComponent } from '../languages/languages.component';
+// Services
+import { TranslationHelper } from '../../shared/translation-helper';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterLink, ContactInfoComponent, PageListComponent, LanguagesComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ContactInfoComponent,
+    PageListComponent,
+    LanguagesComponent,
+    TranslateModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnDestroy {
+  currentLanguage: string = 'en';
 
+  constructor(private translationHelper: TranslationHelper) {
+    this.currentLanguage = this.translationHelper.getCurrentLanguage();
+  }
+
+  ngOnDestroy(): void {
+    this.translationHelper.unsubscribe();
+  }
 }
