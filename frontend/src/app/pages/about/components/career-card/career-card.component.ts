5a822300e78027d46265d726b4bdc8994378e99e
whereishikaru <h.suzuki.028@gmail.com>
2024-12-28 21:10:35 +1300

    refactor: translate the client block on the about page

--- frontend/src/app/pages/about/components/career-card/career-card.component.ts
+++ frontend/src/app/pages/about/components/career-card/career-card.component.ts
@@ -1,19 +1,42 @@
 // Libraries
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { TranslateModule } from '@ngx-translate/core';
 // Services
-import { ICareer, IOffice } from '../../../../../util/interfaces';
+import { LanguageService } from '../../../../shared/language.service';
+import { ICareer } from '../../../../../util/interfaces';
 
 @Component({
   selector: 'app-career-card',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, TranslateModule],
   templateUrl: './career-card.component.html',
   styleUrl: './career-card.component.scss'
 })
 
-export class CareerCardComponent {
+export class CareerCardComponent implements OnInit, OnDestroy {
   @Input() careers?: ICareer[] = [];
   @Input() career?: ICareer | null = null;
+  currentLanguage: string = 'en';
+  private langChangeSubscription!: Subscription;
+
+  constructor(private languageService: LanguageService) { }
+
+  ngOnInit(): void {
+    this.currentLanguage = this.languageService.getCurrentLanguage();
+
+    this.langChangeSubscription = this.languageService.currentLanguage$.subscribe(
+      (lang) => {
+        this.currentLanguage = lang;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.langChangeSubscription) {
+      this.langChangeSubscription.unsubscribe();
+    }
+  }
 }
