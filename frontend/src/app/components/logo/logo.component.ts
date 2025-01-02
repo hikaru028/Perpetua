682a1438a1ef2936000ee0cf90b7e0ea73c1c587
whereishikaru <h.suzuki.028@gmail.com>
2025-01-02 21:48:20 +1300

    refactor: add functions to the logo

--- frontend/src/app/components/logo/logo.component.ts
+++ frontend/src/app/components/logo/logo.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MenuService } from '../../shared/menu.service';
+import { ProjectService } from '../../shared/project.service';
+import { ArticleService } from '../../shared/article.service';
 
 @Component({
   selector: 'app-logo',
@@ -10,10 +12,15 @@ import { MenuService } from '../../shared/menu.service';
   styleUrl: './logo.component.scss'
 })
 export class LogoComponent {
+  projectService: ProjectService = inject(ProjectService);
+  articleService: ArticleService = inject(ArticleService);
+
   constructor(private menuService: MenuService) { }
 
   onLogoClick() {
     this.menuService.resetMenu();
+    this.articleService.sortArticles('all');
+    this.projectService.filterProjects('all');
   }
 
   onKeyDown(event: KeyboardEvent) {
