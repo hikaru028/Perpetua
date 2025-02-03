2755b445eade7ddf717e439a152ea15fb92316c8
whereishikaru <h.suzuki.028@gmail.com>
2025-02-04 10:54:41 +1300

    refactor: change variable

--- frontend/src/app/pages/not-found/not-found.component.ts
+++ frontend/src/app/pages/not-found/not-found.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Meta, Title } from '@angular/platform-browser';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FooterComponent],
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
